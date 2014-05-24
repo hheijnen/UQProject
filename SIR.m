433ef86e3ca818f070b76a7691969e7f1436a672
Stefannn <stefan@public-docking-hg-3561.ethz.ch>
2014-05-24 14:21:03 +0200

    updated SIR.m

--- SIR.m
+++ SIR.m
@@ -1,23 +1,25 @@
 function dy = sir(t, y,par)
 
 %% Old, needs checking
-mu    = par(1);
-alpha = par(2);
-gamma = par(3);
-beta0 = par(4);
-beta1 = par(5);
+alpha    = par(1);
+beta     = par(2);
+tau      = par(3);
+m        = par(4);
+S0       = par(5);
 
 S = y(1);
-E = y(2);
-I = y(3);
-tt = y(4);
+I = y(2);
+R = y(3);
+tt = y(4);  % whats this?
 
 %beta_SEIR depends on time! it is periodic and has to do with the contact
 %rate!
-beta_SEIR = @(beta0,beta1,t) beta0*(1+beta1*cos(2*pi*t));
+%beta_SEIR = @(beta0,beta1,t) beta0*(1+beta1*cos(2*pi*t));
 
+%beta should depend on time: almost zero in summer, big enough in winter...
+beta_SIR = @(beta,t) 1 + 10*t^2 + 28;
 %% New
-lambda = beta_SEIR(beta0,beta1,tt)*(alpha*I + m);
+lambda = beta_SIR(beta,t)*(alpha*I + m);
 
 dS = -lambda*S;
 dI = lambda*S - tau*I;
