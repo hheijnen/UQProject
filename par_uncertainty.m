8e24369365882575c1bfca18ebcdb878d356370a
Yacine <dyacine@student.ethz.ch>
2014-05-27 03:15:34 +0200

    Hessian and parametric uncertainty

--- par_uncertainty.m
+++ par_uncertainty.m
@@ -1,4 +1,4 @@
-% Parametric uncertainty calculation
+% Parametric uncertainty for SIR model
 
 clear all;
 close all;
@@ -8,30 +8,42 @@ load('seasonaldata.mat');
 % optimal value
 alpha = 0.1523;
 tau = 1.7357;
-gamma = 0.5;
 m = 2.7*1e-6;
 S0 = 0.2743;
-E0 = 0.05;
 beta = 50;
 
 theta(1) = alpha;
 theta(2) = tau;
-theta(3) = gamma;
-theta(4) = m;
-theta(5) = S0;
-theta(6) = E0;
+theta(3) = m;
+theta(4) = S0;
+
 par0 = theta;
 
-[parF,fval] = fminsearch(@(par) diff_sqr_SEIR(par),par0);
+% options =  optimset('MaxIter',1000,'MaxFunEvals',2000);
 
-theta_opt = parF
+% Calculation of the optimal parameters
 
-options = optimset('TolFun', 1e-6);
-options = optimset('TolX', 1e-6);
-[parF2,fval2,exitflag,output,grad,hessian] = fminunc(@(par) diff_sqr(par), parF);
+[parF,fval] = fminsearch(@(par) diff_sqr(par),par0);
+parF
+
+% Calculation of the hessian
+[parF2, fval2, exitflag, output, grad, hessian] = fminunc(@(par) diff_sqr(par), parF);
+
+N = length(seasonaldata(:,1));
+sigma = sqrt( 1/N * diff_sqr(parF));
+H = 1/(2*sigma^2) * hessian;
+uncertainty = inv(H)
+
+
+% % Hessian
+% N = length(seasonaldata(:,1));
+% sigma = sqrt( 1/(N-1) * diff_sqr(parF));
 % [hess, err] = hessian(@(par) diff_sqr(par), parF);
+% H = 1/(2*sigma^2) * hess
 
-hessian
+
+% % [parF2,fval2,exitflag,output,grad,hessian] = fminunc(@(par) diff_sqr(par), parF);
+% % hessian
 
 
 
